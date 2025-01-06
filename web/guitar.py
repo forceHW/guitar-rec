e6b0a6cfe151dd3285bfe0796e246c183bdaade6
unknown <hoodhwaleedh@gmail.com>
2025-01-05 19:53:16 -0500

    proof of concept

---
+++ web/guitar.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+# API_KEY = os.getenv('API_KEY')
+# openai.api_key = API_KEY
+
+client = OpenAI(api_key = os.getenv('API_KEY'))
+
+
+
+budget = input('what is your budget: ')
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role" : "system",
+            "content" : "In response to the user, we will display a list of about 10-20 guitars that fit within the given budget."
+        },
+        {
+            "role": "user",
+            "content": f"Can you recommend me a begginer guitar with a budget of {budget}",
+        }
+    ],
+    model="gpt-4o-mini",
+)
+
+
+print(chat_completion.choices[0].message)
+    
+
+

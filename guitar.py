24c397d86302aa7e12280b6b7af2772b54811727
unknown <hoodhwaleedh@gmail.com>
2025-01-09 19:25:09 -0500

    some flask additions

--- guitar.py
+++ guitar.py
@@ -1,8 +1,10 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from flask import Flask, render_template, request
 
 load_dotenv()
+app = Flask(__name__)
 
 
 
@@ -12,22 +14,38 @@ client = OpenAI(api_key = os.getenv('API_KEY'))
 
 budget = input('what is your budget: ')
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role" : "system",
-            "content" : "In response to the user, we will display a list of about 10-20 guitars that fit within the given budget."
-        },
-        {
-            "role": "user",
-            "content": f"Can you recommend me a begginer guitar with a budget of {budget}",
-        }
-    ],
-    model="gpt-4o-mini",
-)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+
+    if request.method == 'POST':
+            min_budget = request.form.get('min-budget')
+            max_budget = request.form.get('max-budget')
+            genre = request.form.get('Genre/Tone')
+            shape = request.form.get('shape')
 
 
-print(chat_completion.choices[0].message)
+    chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": "In response to the user, we will display a list of about 10-20 guitars that fit within the given budget."
+                },
+                {
+                    "role": "user",
+                    "content": f"Can you recommend me a beginner guitar with a budget range of {min_budget} to {max_budget}, "
+                               f"for playing {genre} music, and with a preferred shape of '{shape}'?"
+                }
+            ],
+            model="gpt-4o-mini",
+        )
+    # print(chat_completion.choices[0].message['content'])
+
+
+    return render_template('index.html', guitars=chat_completion.choices[0].message['content'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
     
 
 

41a1421f42f630ac4b36fa0c5c82fb8d7721d948
unknown <hoodhwaleedh@gmail.com>
2025-01-09 19:32:18 -0500

    geek

--- guitar.py
+++ guitar.py
@@ -17,32 +17,34 @@ budget = input('what is your budget: ')
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    guitars = []
 
     if request.method == 'POST':
-            min_budget = request.form.get('min-budget')
-            max_budget = request.form.get('max-budget')
-            genre = request.form.get('Genre/Tone')
-            shape = request.form.get('shape')
+        min_budget = request.form.get('min-budget')
+        max_budget = request.form.get('max-budget')
+        genre = request.form.get('Genre/Tone')
+        shape = request.form.get('shape')
 
 
-    chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content": "In response to the user, we will display a list of about 10-20 guitars that fit within the given budget."
-                },
-                {
-                    "role": "user",
-                    "content": f"Can you recommend me a beginner guitar with a budget range of {min_budget} to {max_budget}, "
-                               f"for playing {genre} music, and with a preferred shape of '{shape}'?"
-                }
-            ],
-            model="gpt-4o-mini",
-        )
-    # print(chat_completion.choices[0].message['content'])
+        chat_completion = client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "In response to the user, we will display a list of about 10-20 guitars that fit within the given budget."
+                    },
+                    {
+                        "role": "user",
+                        "content": f"Can you recommend me a beginner guitar with a budget range of {min_budget} to {max_budget}, "
+                                f"for playing {genre} music, and with a preferred shape of '{shape}'?"
+                    }
+                ],
+                model="gpt-4o-mini",
+            )
+
+        guitars = chat_completion.choices[0].message['content'].split('\n')
 
 
-    return render_template('index.html', guitars=chat_completion.choices[0].message['content'])
+    return render_template('index.html', guitars=guitars)
 
 if __name__ == '__main__':
     app.run(debug=True)
